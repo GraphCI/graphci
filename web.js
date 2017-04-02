a76a750370bea237f3b135c3c9594fd33ddbac93
Eli Mydlarz <eli.mydlarz@gmail.com>
2017-04-02 22:14:52 +1000

    Parsing request body correctly

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.argv[2] || 80;
 
 app.post('/github/build', (req, res) => {
-  const urlToCheckout = req.commits[0].url;
+  const urlToCheckout = req.body.commits[0].url;
   console.info(urlToCheckout);
   res.sendStatus(200);
 });
