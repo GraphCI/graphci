e0090e203430334db22c9a16f81f76b086dc693f
Eli Mydlarz <eli.mydlarz@gmail.com>
2017-04-02 22:27:39 +1000

    Using body-parser

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 #! /usr/bin/env node
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.argv[2] || 80;
+
+app.use(bodyParser.json());
 
 app.post('/github/build', (req, res) => {
   const urlToCheckout = req.body.commits[0].url;
