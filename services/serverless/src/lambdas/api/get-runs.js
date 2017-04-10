23f38455ae9d6e0cfb62c384da2cb182b102e91d
Ryan Boucher <ryan.boucher@distributedlife.com>
2017-04-10 13:48:49 +1000

    Try setting a delimiter

--- services/serverless/src/lambdas/api/get-runs.js
+++ services/serverless/src/lambdas/api/get-runs.js
@@ -4,8 +4,8 @@ const s3 = new AWS.S3();
 
 const params = {
   Bucket: 'dockercise',
+  delimiter: '/',
   EncodingType: 'url',
-  FetchOwner: false,
 };
 
 module.exports = () => s3.listObjectsV2(params).promise();
