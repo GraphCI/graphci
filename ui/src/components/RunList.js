c09aa124a36bff1d6c8c11766cd366b167dcaee6
Ryan Boucher <ryan.boucher@distributedlife.com>
2017-04-25 10:06:56 +1000

    a little bit of run list style

--- ui/src/components/RunList.js
+++ ui/src/components/RunList.js
@@ -3,16 +3,27 @@ import moment from 'moment';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+const runStyle = {
+  padding: 10,
+};
+
 const RunOverview = ({ run }) => (
   <Link to={`/web/run/${run}`} style={{ display: 'block' }}>
-    <span> RunId: {run}</span>
-    <span> Date: {moment(run).format('dddd DD of MMMM, YYYY  @ HH:MM (Z)')}</span>
-    <span> Started: {moment(run).fromNow()}</span>
+    <div style={runStyle}>
+      <span>{moment(run).fromNow()}</span>
+      <span> RunId: {run}</span>
+      <span> Date: {moment(run).format('dddd DD of MMMM, YYYY  @ HH:MM (Z)')}</span>
+    </div>
   </Link>
 );
 
+const list = {
+  maxWidth: 800,
+  margin: '0 auto',
+};
+
 const RunListView = ({ runs }) => (
-  <div>
+  <div style={list}>
     {runs.map((run) => (<RunOverview key={run} run={Number(run)} />))}
   </div>
 );
