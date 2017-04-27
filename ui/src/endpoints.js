9e3b00f97efd693086f577ade230d8695ff74c5f
Ryan Boucher <ryan.boucher@distributedlife.com>
2017-04-28 00:12:33 +1000

    Update URL of API

--- ui/src/endpoints.js
+++ ui/src/endpoints.js
@@ -1,4 +1,4 @@
-const apiBase = 'https://qagia23cyk.execute-api.ap-southeast-2.amazonaws.com/Prod/';
+const apiBase = 'https://ilc5hajy2b.execute-api.ap-southeast-2.amazonaws.com/Prod/';
 
 export const runsUrl = () => `${apiBase}/api/v1/runs`;
 export const runUrl = (runId) => `${apiBase}/api/v1/run/${runId}`;
